446183abccb67c8375923c333e4da3ac7f1efb67
pommatt <pommatt@gmail.com>
2019-05-01 22:54:54 +0700

    initial project Serilog

---
+++ src/AspnetCoreWithSerilog/BuildingBlock/WebHostBuilderExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+namespace AspnetCoreWithSerilog.BuildingBlock
+{
+    public static class WebHostBuilderExtension
+    {
+        public static IWebHostBuilder UseDefaultLogger(this IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder.UseSerilog((ctx, cfg) => cfg.ReadFrom.ConfigurationSection(ctx.Configuration.GetSection("serilog")));
+            return webHostBuilder;
+        }
+    }
+}
